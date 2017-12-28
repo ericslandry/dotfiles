9540ce38f827dcd332f8f78335be86e881d3ad10
Eric Landry <eric.s.landry@gmail.com>
2017-12-28 12:09:30 -0400

    Fixing stuff

--- .dotfiles-setup.sh
+++ .dotfiles-setup.sh
@@ -11,16 +11,21 @@ rm --recursive $HOME/dotfiles-tmp
 function dotfiles {
    /usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME $@
 }
-mkdir -p .dotfiles-backup
+mkdir -p $HOME/.dotfiles/backup
 dotfiles checkout
 if [ $? = 0 ]; then
   echo "Checked out dotfiles.";
   else
     echo "Backing up pre-existing dot files.";
-    dotfiles checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .dotfiles-backup/{}
+    dotfiles checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} $HOME/.dotfiles/backup/{}
 fi;
 dotfiles checkout
+
+source $HOME/.bashrc
+
+# Don't pass --global param to avoid messing up your "real" git
 dotfiles config status.showUntrackedFiles no
+dotfiles config push.default simple
 dotfiles config user.name "$GIT_USER_NAME"
 dotfiles config user.email "$GIT_USER_EMAIL"
 
