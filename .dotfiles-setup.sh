43a9db29633927733b6a926fab3a1588cf13a181
Eric <eric.s.landry@gmail.com>
2017-12-29 13:18:21 -0400

    Fixing things

--- .dotfiles-setup.sh
+++ .dotfiles-setup.sh
@@ -10,16 +10,18 @@ function dotfiles {
    /usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME $@
 }
 
-mkdir -p $HOME/.dotfiles-backup
+mkdir -p $HOME/.dotfiles/backup
 alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
 dotfiles checkout
 if [ $? = 0 ]; then
   echo "Checked out dotfiles.";
 else
   echo "Backing up pre-existing dot files.";
-  dotfiles checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} $HOME/.dotfiles-backup/{}
+  dotfiles checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} $HOME/.dotfiles/backup/{}
 fi;
+echo "Try check out again"
 dotfiles checkout
+
 source $HOME/.bashrc
 
 # Don't pass --global param to avoid messing up your "real" git
